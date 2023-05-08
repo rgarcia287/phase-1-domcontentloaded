ffc90caa9935ae7db71ce9c6e6d3ccb4d792f643
Roberto <robertogarcia287@gmail.com>
2023-05-08 15:13:04 -0500

    Roberto's DOMContent

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   let insideText = document.getElementById('text')
+   insideText.textContent = "This is really cool!"
+  });
